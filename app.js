ef331bd556e43b5e99d1d75027f67dbde38e395b
phanuwit13 <big.phanuwit@gmail.com>
2020-12-08 13:30:05 +0700

    test

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.get('*', (req,res) =>{
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}.`);
+// });
 
 
 const user = require('./routes/user');
